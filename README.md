8a9b35eb2cbc62a71553b0495f325d23ab895021
Ryan Bugden <rbugden@gmail.com>
2024-05-14 00:47:56 -0400

    Update

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # Text Collector
 A RoboFont start-up script that places a button to the left of the main input text dialog in your Space Center. This button, when pressed, saves the current string to the Input Text tab in your RoboFont preferences.
 
-![](./_images/textCollector_demo.png)
+![](./_images/demo.png)
 
 
 ### Versions:
 
 ```
 1.0.0 2020-01-28
+1.2.0 2024-05-14
 ```
--- textCollector.py
+++ textCollector.py
@@ -1,26 +1,20 @@
 import os
 from mojo.UI import CurrentSpaceCenter, preferencesChanged
-from mojo.events import addObserver
+from mojo.subscriber import Subscriber, registerSpaceCenterSubscriber
 from lib.tools.defaults import getDefault, setDefault
 from vanilla import ImageButton
 
-class textCollector:
-	
+class TextCollector(Subscriber):
 	'''
-	Extension that puts a button in Space Center.
+	Start-up script that puts a button in Space Center.
 	This button allows you to save the current 
 	string to your Input Text strings.
 	
 	Ryan Bugden
-	2020.01.28
 	'''
-	
-	def __init__(self):
-		self.resources_path = os.path.abspath("./resources")
-		addObserver(self, "makeButton", "spaceCenterDidOpen")
 		
-	def makeButton(self, notification):
-		csc = CurrentSpaceCenter()
+	def spaceCenterWillOpen(self, notification):
+		csc = notification['spaceCenter']
 		gutter = 10
 		b_w = 20
 		inset_b = 1
@@ -31,36 +25,29 @@ class textCollector:
 		csc.top.glyphLineInput.setPosSize((l + b_w + gutter, t, w, h))
 		l, t, w, h = csc.top.glyphLineInput.getPosSize()
 		
-		csc.save = ImageButton(
+		image_path = os.path.join(os.path.dirname(__file__), "resources", "_icon_Save.pdf")
+		csc.top.save = ImageButton(
 		  (l - gutter - b_w, t + inset_b, b_w, b_h), 
-		  imagePath = self.resources_path + '/_icon_Save.pdf',
-		  callback = self.saverCallback, 
+		  imagePath = image_path,
+		  callback = self.saver_callback, 
 		  sizeStyle = 'regular'
 		  )
-		csc.save.getNSButton().setBordered_(0)
-		csc.save.getNSButton().setBezelStyle_(2)
+		csc.top.save.getNSButton().setBordered_(0)
+		csc.top.save.getNSButton().setBezelStyle_(2)
+		# Set as a template image to handle dark mode well
+		csc.top.save.getNSButton().image().setTemplate_(True)
 		
-	def saverCallback(self, sender):
-		print("saving input text")
+
+	def saver_callback(self, sender):
 		csc = CurrentSpaceCenter()
-		
 		string = csc.getRaw()
-		print("string is", string)
-		
 		before = getDefault("spaceCenterInputSamples")
-		print("before is ", before)
 		after = list(before)
-		print("after is ", after)
 		if string not in after:
 			after.append(string)
-			print("after2 is ", after)
 			setDefault("spaceCenterInputSamples", after)
-			print("saved input text.")
-		else:
-			print("did nothing.")
-		
-		preferencesChanged()
+			preferencesChanged()
 		
 		 
-textCollector()
+registerSpaceCenterSubscriber(TextCollector)
 	
